7a9058861fb070d888d41f2879cc6cbfe27d9a70
T72B <ii311205@yandex.ru>
2018-11-03 13:05:31 +0000

    add new function

--- main.py
+++ main.py
@@ -14,8 +14,13 @@ def func3(x=0, y=0):
     return x*y
 
 
+def func4(x=0, y=0, z=0):
+    return (x+y)*2 -z
+
 x = 65
 y = 48
+z = 35
 print("sum: {}".format(func1(x, y)))
 print("minus: {}".format(func2(y, x)))
 print("multiplex: {}".format(func3(x, y)))
+print("function (x+y)*2 - z = {}".format(func4(x,y,z)))
